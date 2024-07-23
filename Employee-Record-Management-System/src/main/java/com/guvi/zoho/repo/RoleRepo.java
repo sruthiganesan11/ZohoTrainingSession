15654d3698a6f86788139efbd8acd895891f1832
Sruthi <sruthiganesan11@gmail.com>
2024-07-23 19:35:48 +0530

    Zoho Training session assignment backend

--- Employee-Record-Management-System/src/main/java/com/guvi/zoho/repo/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.guvi.zoho.repo;
-
-import com.guvi.zoho.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
